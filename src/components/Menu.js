b14411e1875be9bfee633d97ec6322a4c9b195c7
Louis Moselhi <moselhil@gmail.com>
2018-10-30 10:19:28 +0000

    add starter div inside wrapper

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,9 +13,26 @@ const MenuWrapper = styled.div`
 
 MenuWrapper.displayName = 'MenuWrapper';
 
+const Starters = styled.div`
+  width: 50%;
+  display: flex;
+  border: 0.1rem solid black;
+  flex-direction: column;
+  text-color: black;
+  border-radius: 1.25rem;
+  padding: 1rem;
+  margin-top: 1rem;
+`;
+
+Starters.displayName = 'Starters';
+
 class Menu extends React.Component {
   render() {
-    return <MenuWrapper />;
+    return (
+      <MenuWrapper>
+        <Starters />
+      </MenuWrapper>
+    );
   }
 }
 export default Menu;
