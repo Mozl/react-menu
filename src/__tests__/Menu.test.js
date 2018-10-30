7be342d95669fb85d3afac774e5b04a2542316db
Louis Moselhi <moselhil@gmail.com>
2018-10-30 09:35:01 +0000

    add Menu test, tests folder

--- src/__tests__/Menu.test.js
+++ src/__tests__/Menu.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Menu from '../components/Menu';
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Menu />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 it('renders correctly', () => {
-  const tree = renderer.create(<App />);
+  const tree = renderer.create(<Menu />);
   expect(tree).toMatchSnapshot();
 });
